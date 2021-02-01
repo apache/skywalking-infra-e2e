9560a9db180d03a485bfe94272a5a3e58d72fa7a
HumbertZhang <504490160@qq.com>
2021-02-01 14:38:22 +0800

    add license header

--- internal/config/globalConfig.go
+++ internal/config/globalConfig.go
@@ -1,8 +1,28 @@
+//
+// Licensed to Apache Software Foundation (ASF) under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Apache Software Foundation (ASF) licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package config
 
 import (
 	"fmt"
 	"io/ioutil"
+
+	"github.com/apache/skywalking-infra-e2e/internal/logger"
 
 	"gopkg.in/yaml.v2"
 
@@ -19,7 +39,8 @@ var GlobalConfig GlobalE2EConfig
 
 func ReadGlobalConfigFile(configFilePath string) error {
 	if GlobalConfig.Ready {
-		return fmt.Errorf("can not read e2e config repeatedly")
+		logger.Log.Info("e2e config has been initialized")
+		return nil
 	}
 
 	e2eFile := configFilePath
