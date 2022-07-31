469504f9c5016a6751a81fd1478cb754d0758f95
Wang Zichen <1572839226@qq.com>
2022-07-31 09:49:30 +0800

    polished

--- commands/verify/verify.go
+++ commands/verify/verify.go
@@ -88,36 +88,61 @@ func verifySingleCase(expectedFile, actualFile, query string) error {
 	return nil
 }
 
+// ErrContain has two fields. The errAddr field is a pointer points to Err. Err's type is "multierror.Error".
+// When an error occurs during concurrently verifying cases, the process will append the new error to Err.Errors.
+// The Err.Errors is []error, which contains all errors occurring during the verification. errAddr points to Err.
 type ErrContain struct {
 	errAddr *multierror.Error
 	mutex   sync.Mutex
 }
 
-func concurrentVerifySingleCase(idx int, v config.VerifyCase, errContain *ErrContain, retry int, interval time.Duration, wg *sync.WaitGroup) {
+// verifyInfo contains necessary information about verification
+type verifyInfo struct {
+	retryCount int
+	interval   time.Duration
+	failFast   bool
+}
+
+func concurrentSafeErrAppend(errContain *ErrContain, err error) {
+	errContain.mutex.Lock()
+	errContain.errAddr = multierror.Append(errContain.errAddr, err)
+	errContain.mutex.Unlock()
+}
+
+func concurrentVerifySingleCase(idx int, v config.VerifyCase, errContain *ErrContain, verify verifyInfo, wg *sync.WaitGroup, chanBool chan bool) {
 	if v.GetExpected() == "" {
 		errMsg := fmt.Sprintf("the expected data file for case[%v] is not specified\n", idx)
 		logger.Log.Warnf(errMsg)
-		errContain.mutex.Lock()
-		errContain.errAddr = multierror.Append(errContain.errAddr, errors.New(errMsg))
-		errContain.mutex.Unlock()
+		concurrentSafeErrAppend(errContain, errors.New(errMsg))
+		if verify.failFast {
+			chanBool <- true
+		}
 		wg.Done()
 		return
 	}
 
-	for current := 1; current <= retry; current++ {
+	for current := 1; current <= verify.retryCount; current++ {
 		if err := verifySingleCase(v.GetExpected(), v.GetActual(), v.Query); err == nil {
 			break
-		} else if current != retry {
+		} else if current != verify.retryCount {
 			logger.Log.Warnf("verify case[%v] failure, will continue retry, %v", idx, err)
-			time.Sleep(interval)
+			time.Sleep(verify.interval)
 		} else {
-			errContain.mutex.Lock()
-			errContain.errAddr = multierror.Append(errContain.errAddr, err)
-			errContain.mutex.Unlock()
+			concurrentSafeErrAppend(errContain, err)
+			if verify.failFast {
+				chanBool <- true
+			}
 		}
 	}
-
 	wg.Done()
+}
+
+func checkForRetryCount(retryCount int) int {
+	if retryCount <= 0 {
+		return 1
+	}
+
+	return retryCount
 }
 
 // DoVerifyAccordingConfig reads cases from the config file and verifies them.
@@ -129,9 +154,8 @@ func DoVerifyAccordingConfig() error {
 	e2eConfig := config.GlobalConfig.E2EConfig
 
 	retryCount := e2eConfig.Verify.RetryStrategy.Count
-	if retryCount <= 0 {
-		retryCount = 1
-	}
+	retryCount = checkForRetryCount(retryCount)
+
 	interval, err := parseInterval(e2eConfig.Verify.RetryStrategy.Interval)
 	if err != nil {
 		return err
@@ -148,10 +172,25 @@ func DoVerifyAccordingConfig() error {
 
 	if concurrency {
 		var waitGroup sync.WaitGroup
+		VerifyInfo := verifyInfo{
+			retryCount,
+			interval,
+			failFast,
+		}
+		chanBool := make(chan bool, 1)
 		waitGroup.Add(len(e2eConfig.Verify.Cases))
 
 		for idx, v := range e2eConfig.Verify.Cases {
-			go concurrentVerifySingleCase(idx, v, &errContain, retryCount, interval, &waitGroup)
+			go concurrentVerifySingleCase(idx, v, &errContain, VerifyInfo, &waitGroup, chanBool)
+		}
+
+		if failFast {
+			for {
+				result := <-chanBool
+				if result {
+					return errContain.errAddr.ErrorOrNil()
+				}
+			}
 		}
 
 		waitGroup.Wait()
